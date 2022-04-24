e9b9990e177de5e15519f88ad7d6f8eeae59e4ff
grecha1337 <4rmor3d@gmail.com>
2022-04-24 15:18:57 +0300

    Перенос SettingsForm на функциональный компонент

--- src/components_refact/index.ts
+++ src/components_refact/index.ts
@@ -1,5 +1,6 @@
 import React from "react";
 
 import { ListErrors } from "./ui_lib/list-error";
+import { SettingsForm } from "./settings-form";
 
-export { ListErrors };
+export { ListErrors, SettingsForm };
