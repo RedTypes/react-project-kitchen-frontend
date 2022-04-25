48b3495f31778c4a454d56dfb4ca2091f28860cf
Ilya Pichugin <ilia-pichugin2014@yandex.ru>
2022-04-26 02:00:01 +0300

    articles, profile, forms on redux-toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import { articlesReducer, getArticlesSuccess, getTagsSuccess } from './articlesSlice';
+import { profileReducer, loginSuccess, logoutUser } from './profileSlice';
+import { formsReducer } from './formsSlice/formsReducer';
+// import { changeEmail, changePassword, changeUsername } from './formsSlice/registerFormSlice';
+// import { changeEmail, changePassword } from './formsSlice/loginFormSlice';
