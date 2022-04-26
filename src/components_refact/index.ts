4782f0301d168fdd5783b33f9438398ac1e2f55b
grecha1337 <4rmor3d@gmail.com>
2022-04-26 21:44:23 +0300

    Рефактор компонента comment-input

--- src/components_refact/index.ts
+++ src/components_refact/index.ts
@@ -1,5 +1,6 @@
 import React from "react";
 
 import { ListErrors } from "./ui_lib/list-error";
+import CommentInput from "./comment-input"
 
-export { ListErrors };
+export { ListErrors, CommentInput };
