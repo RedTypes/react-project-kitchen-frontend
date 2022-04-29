46a26cc7bb6e8052a30f05a2c1b36d3c5fb430ed
Evgeny Karpel <kspshnik@yandex.ru>
2022-04-29 15:09:21 +0300

    [Refactor] getUserProfileThunk. Типизация, сообщение об ошибке, etc

--- src/thunks/index.ts
+++ src/thunks/index.ts
@@ -1,13 +1,13 @@
 import loadInitialDataThunk from './load-initial-data-thunk';
 import { deleteLikeThunk, addLikeThunk } from './delete-add-likes-thunks';
-import { userDataThunk } from './load-user-data-thunk';
+import getUserProfileThunk from './get-user-profile-thunk';
 import { followProfileThunk, unfollowProfileThunk } from './post-follow-unfollow--profile-thunk';
 // eslint-disable-next-line import/prefer-default-export
 export {
   loadInitialDataThunk,
   deleteLikeThunk,
   addLikeThunk,
-  userDataThunk,
+  getUserProfileThunk,
   unfollowProfileThunk,
   followProfileThunk,
 };
